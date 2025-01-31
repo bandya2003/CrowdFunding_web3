8e9f6127de85fcb0763f9c59788bb2147aa064a6
bandya2003 <61108642+bandya2003@users.noreply.github.com>
2025-01-31 11:41:52 +0530

    Delete pages directory

--- pages/index.js
+++
@@ -1,6 +0,0 @@
-// used for the exporting the pages
-
-export {default as Home} from './Home';
-export {default as Profile} from './Profile';
-export {default as CreateCampaign} from './CreateCampaign';
-export {default as CampaignDetails} from './CampaignDetails';
